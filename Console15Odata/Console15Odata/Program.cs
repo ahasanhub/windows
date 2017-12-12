ebc0b6427c40f94d86d0366b6213811c2c190cf7
Ahasan <shobuz.cse03@gmail.com>
2017-12-12 20:18:18 +0600

    Update from home

---
+++ Console15Odata/Console15Odata/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Console15Odata.OdataService;
+
+namespace Console15Odata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NAV nav=new NAV(new Uri(@"http://ahasan-pc:7048/DynamicsNAV100/OData/Company('CRONUS%20International%20Ltd.')"));
+            nav.Credentials=new NetworkCredential(@"ahasan-pc\ahasan","shobuz");
+            var data = (nav.CustomerService.Take(100)).ToList();
+        }
+    }
+}
