7d5cbb02c6621a3b0268b68779c110de3f7a12b8
Zayn An Nisa <shobuz_cse03@yahoo.com>
2018-08-13 23:43:19 +0600

    update from home

---
+++ Windows Service/MyWindowsServiceApp/MyWindowsServiceApp/MyNewService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWindowsServiceApp
+{
+    public partial class MyNewService : ServiceBase
+    {
+        private System.Diagnostics.EventLog eventLog;
+        private int eventId = 1;
+        public MyNewService()
+        {
+            InitializeComponent();
+            eventLog = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                                "MySource", "MyNewLog");
+            }
+            eventLog.Source = "MySource";
+            eventLog.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog.WriteEntry("IN OnStart");
+            // Set up a timer to trigger every minute.  
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 10000; // 60 seconds  
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+        public void OnTimer(object sender, System.Timers.ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.  
+            eventLog.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+        }
+        protected override void OnStop()
+        {
+            eventLog.WriteEntry("In onStop.");
+        }
+    }
+}
---
+++ Windows Service/MyWindowsServiceApp/MyWindowsServiceApp/README.txt
@@ -0,0 +1,3 @@
+﻿1. Follow this link for create windows service 
+https://docs.microsoft.com/en-us/dotnet/framework/windows-services/walkthrough-creating-a-windows-service-application-in-the-component-designer
+2. Run into visual studio developer console with administrator mode: installutil.exe "C:\Github\Projects\windows\Windows Service\MyWindowsServiceApp\MyWindowsServiceApp\bin\Debug\MyWindowsServiceApp.exe"
